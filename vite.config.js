61e5f625fc7b2ffb79c1b1b0b5ddc62eaa9b7c8a
Joel_Vegas_Romero <j.vegasromero.2004@gmail.com>
2026-05-20 20:24:59 +0200

    db

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "https://backend-kioskohub.onrender.com/",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
+      },
+    },
+  },
 });
